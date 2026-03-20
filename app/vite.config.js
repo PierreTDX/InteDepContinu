a6ad3f09eee08a4d55c7c6dca9a416e4b28be3e9
PierreTDX <pierre.tondeux@gmail.com>
2026-03-20 13:08:30 +0100

    watch chagment front pour éviter de restart le container docker

--- app/vite.config.js
+++ app/vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_');
   return {
     plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
     base: '/InteDepContinu/',
     define: {
       'process.env.VITE_USE_MOCK_API': JSON.stringify(env.VITE_USE_MOCK_API || ''),
